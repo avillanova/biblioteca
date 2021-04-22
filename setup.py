cf21258c222916189bbd863205b992e0eb541956
alex.villanova <alex.villanova@mirumagency.com>
2021-04-22 16:29:10 -0300

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 setup(
     name='biblioteca',
-    version='0.0.2',
+    version='0.0.3',
     description='My first Python library',
     author='Me',
     license='MIT',
