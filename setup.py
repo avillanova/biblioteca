58c7b39cb81d9d3f949538ecc367e32f0136d409
alex.villanova <alex.villanova@mirumagency.com>
2021-04-22 15:19:31 -0300

    First commit

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 setup(
     name='mypythonlib',
-    version= '0.1.0',
+    version='0.1.1',
     description='My first Python library',
     author='Me',
     license='MIT',
