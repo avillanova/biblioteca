9db556a6f4a16830e33509bf830aa4d956d2dce7
alex.villanova <alex.villanova@mirumagency.com>
2021-04-22 15:05:45 -0300

    ok

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 setup(
     name= 'mypythonlib',
-    packages=find_packages(),
+    packages=find_packages(include=['mypythonlib']),
     version= '0.1.0',
     description='My first Python library',
     author='Me',
